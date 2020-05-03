998f997324935d97abb8771ec86f2564dccb6f38
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-05-03 14:23:26 -0400

    Updates

---
+++ precache-manifest.306d5152f67fc44af079c6f67446fb0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b8622fe8fdef08415518085500e64a",
+    "url": "/exercise-app/index.html"
+  },
+  {
+    "revision": "bd04594e5b8bd9d10be6",
+    "url": "/exercise-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "bd04594e5b8bd9d10be6",
+    "url": "/exercise-app/static/js/2.609c3bef.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercise-app/static/js/2.609c3bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9433723fb8523fad9b7",
+    "url": "/exercise-app/static/js/main.b7cda01f.chunk.js"
+  },
+  {
+    "revision": "dddd7c7083b2ee4e7b2b",
+    "url": "/exercise-app/static/js/runtime-main.cd1d189e.js"
+  }
+]);
